296f624df7c5ae74ecc33e2f88a6deb3ff6b4114
nhontran1 <nhon.tran1@tiki.vn>
2022-11-26 12:51:48 +0700

    add_model

--- migrations/myenvi.sql
+++ migrations/myenvi.sql
@@ -1,19 +1,3 @@
-DROP TABLE IF EXISTS `albums`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `albums` (
-                          `album_ID` int NOT NULL AUTO_INCREMENT,
-                          `name` varchar(40) NOT NULL,
-                          `artist_ID` int NOT NULL,
-                          `created_at` date DEFAULT NULL,
-                          `upload_at` date DEFAULT NULL,
-                          `cover_img` varchar(400) DEFAULT NULL,
-                          PRIMARY KEY (`album_ID`),
-                          KEY `artist_ID` (`artist_ID`),
-                          CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`artist_ID`) REFERENCES `artists` (`artist_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 DROP TABLE IF EXISTS `reports`;
 
 CREATE TABLE `reports` (
@@ -21,7 +5,8 @@ CREATE TABLE `reports` (
     `title` varchar(500) NOT NULL,
     `description` text NOT NULL,
     `created_at` date DEFAULT NULL,
-    `update_at` date DEFAULT NULL,
+    `updated_at` date DEFAULT NULL,
+    `deleted_at` date DEFAULT NULL,
     `status` varchar(20) NOT NULL,
     `author` varchar(100) NOT NULL,
     `lag` float(20,15) NOT NULL,
@@ -33,9 +18,10 @@ CREATE TABLE `reports` (
 DROP TABLE IF EXISTS `images`;
 
 CREATE TABLE `images` (
-    `id` int NOT NULL AUTO_INCREMENT,
+    `id` varchar(100) NOT NULL,
     `report_id` int,
-    `url` varchar(200) NOT NULL,
+    `url` text NOT NULL,
+    `status` int,
     PRIMARY KEY (`id`),
 )
 
@@ -46,6 +32,7 @@ CREATE TABLE `users` (
     `user_name` varchar(50) NOT NULL,
     `pass_word` varchar(50) NOT NULL,
     `is_resolver` bool,
-    PRIMARY KEY (`id`),
-    
+    `email` varchar(50) NOT NULL,
+    `phone` varchar(10),
+    PRIMARY KEY (`id`),   
 )
--- models/models.go
+++ models/models.go
@@ -6,164 +6,109 @@ import (
 	"gorm.io/gorm"
 )
 
-type Albums struct {
+type Reports struct {
 	*gorm.Model
-	AlbumID   uint      `gorm:"column:album_ID"`
-	Name      string    `gorm:"column:name"`
-	ArtistID  uint      `gorm:"column:artist_ID"`
-	Artists   Artists   `gorm:"foreignKey:ArtistID;references:ID"`
-	CreateAt  time.Time `gorm:"column:create_at"`
-	UploadAt  time.Time `gorm:"column:upload_at"`
-	DeletedAt time.Time `gorm:"column:deleted_at"`
-	CoverImg  string    `gorm:"column:cover_img"`
+	ID          int        `json:"id" gorm:"column:id"`
+	Title       string     `json:"title" gorm:"column:title"`
+	Description string     `json:"description" gorm:"column:description"`
+	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt   *time.Time `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at" gorm:"column:deleted_at"`
+	Status      string     `json:"status" gorm:"column:status"`
+	Author      string     `json:"author" gorm:"column:author"`
+	Lag         float32    `json:"lag" gorm:"column:lag"`
+	Lng         float32    `json:"lng" gorm:"column:lng"`
+	ResolverID  int        `json:"resolver_id" gorm:"column:resolver_id"`
 }
 
-type Artists struct {
+type Images struct {
 	*gorm.Model
-	ArtistID    uint      `gorm:"column:artist_ID"`
-	Name        string    `gorm:"column:name"`
-	Description string    `gorm:"column:description"`
-	CreateAt    time.Time `gorm:"column:create_at"`
-	UploadAt    time.Time `gorm:"column:upload_at"`
-	DeletedAt   time.Time `gorm:"column:deleted_at"`
-	CoverImg    string    `gorm:"column:cover_img"`
+	ID       string `json:"id" gorm:"column:id"`
+	ReportID int    `json:"report_id" gorm:"column:report_id"`
+	Url      string `json:"url" gorm:"column:url"`
+	Status   int    `json:"status" gorm:"column:status"`
 }
 
-type Interactions struct {
+type Users struct {
 	*gorm.Model
-	InteractionID uint      `gorm:"column:interaction_ID"`
-	UserID        uint      `gorm:"column:user_ID"`
-	Users         Accounts  `gorm:"foreignKey:Id;references:ID"`
-	SongID        uint      `gorm:"column:song_ID"`
-	Songs         Songs     `gorm:"foreignKey:SongID;references:ID"`
-	Liked         uint      `gorm:"column:liked"`
-	PlayDuration  time.Time `gorm:"column:play_duration"`
-	CreateAt      time.Time `gorm:"column:create_at"`
-	UploadAt      time.Time `gorm:"column:upload_at"`
-	DeletedAt     time.Time `gorm:"column:deleted_at"`
+	ID         string `json:"id" gorm:"column:id"`
+	UserName   string `json:"user_name" gorm:"column:user_name"`
+	PassWord   string `json:"pass_word" gorm:"column:pass_word"`
+	IsResolver bool   `json:"is_resolver" gorm:"column:is_resolver"`
+	Email      string `json:"email" gorm:"column:email"`
+	Phone      string `json:"phone" gorm:"column:phone"`
 }
 
-type PlayLists struct {
-	*gorm.Model
-	Id        uint      `gorm:"column:id"`
-	Name      string    `gorm:"column:name"`
-	CreateAt  time.Time `gorm:"column:create_at"`
-	UploadAt  time.Time `gorm:"column:upload_at"`
-	DeletedAt time.Time `gorm:"column:deleted_at"`
-	CoverImg  string    `gorm:"column:cover_img"`
-	UserID    uint      `gorm:"column:user_ID"`
-	Users     Accounts  `gorm:"foreignKey:Id;references:ID"`
-}
+// type AccountStatus string
 
-type PlayListSongs struct {
-	*gorm.Model
-	SongID     uint      `gorm:"column:song_ID"`
-	Songs      Songs     `gorm:"foreignKey:SongID;references:ID"`
-	PlayListID uint      `gorm:"column:playlist_ID"`
-	PlayLists  PlayLists `gorm:"foreignKey:Id;references:ID"`
-}
+// const (
+// 	Active  AccountStatus = "Active"
+// 	Blocked AccountStatus = "Blocked"
+// )
 
-type Songs struct {
-	*gorm.Model
-	SongID      uint      `gorm:"column:song_ID"`
-	Name        string    `gorm:"column:name"`
-	AlbumID     uint      `gorm:"column:album_ID"`
-	Albums      Albums    `gorm:"foreignKey:AlbumID;references:ID"`
-	ArtistID    uint      `gorm:"column:artist_ID"`
-	Artists     Artists   `gorm:"foreignKey:ArtistID;references:ID"`
-	Lyrics      string    `gorm:"column:lyrics"`
-	Length      uint      `gorm:"column:length"`
-	TrackNumber uint      `gorm:"column:track_number"`
-	CreateAt    time.Time `gorm:"column:create_at"`
-	UploadAt    time.Time `gorm:"column:upload_at"`
-	DeletedAt   time.Time `gorm:"column:deleted_at"`
-	YoutubeLink string    `gorm:"column:youtube_link"`
-}
+// type AccountRole string
 
-type Accounts struct {
-	*gorm.Model
-	Id        uint          `gorm:"column:id"`
-	UserName  string        `gorm:"index:username_idx_uni,unique"`
-	Email     string        `gorm:"column:email"`
-	Password  string        `gorm:"column:password"`
-	CreatedAt time.Time     `gorm:"column:created_at"`
-	UpdatedAt time.Time     `gorm:"column:updated_at"`
-	DeletedAt time.Time     `gorm:"column:deleted_at"`
-	Role      AccountRole   `gorm:"column:role"`
-	GroupType uint          `gorm:"column:group_type"`
-	Status    AccountStatus `gorm:"column:status"`
-}
+// const (
+// 	SuperAdmin AccountRole = "SuperAdmin"
+// 	Admin      AccountRole = "Admin"
+// 	User       AccountRole = "User"
+// )
 
-type AccountStatus string
+// var (
+// 	priority = map[AccountRole]int{
+// 		SuperAdmin: 0,
+// 		Admin:      -1,
+// 		User:       -2,
+// 	}
+// )
 
-const (
-	Active  AccountStatus = "Active"
-	Blocked AccountStatus = "Blocked"
-)
+// func (a AccountRole) CanChange(ar AccountRole) bool {
+// 	return priority[a] > priority[ar]
+// }
 
-type AccountRole string
+// func (a AccountRole) IsValid() bool {
+// 	_, ok := priority[a]
+// 	return ok
+// }
 
-const (
-	SuperAdmin AccountRole = "SuperAdmin"
-	Admin      AccountRole = "Admin"
-	User       AccountRole = "User"
-)
+// // Status enum
+// type Status byte
 
-var (
-	priority = map[AccountRole]int{
-		SuperAdmin: 0,
-		Admin:      -1,
-		User:       -2,
-	}
-)
+// const (
+// 	InitialStatus = iota
+// 	WaitingForScanStatus
+// 	ScanningStatus
+// 	ScannedStatus
+// )
 
-func (a AccountRole) CanChange(ar AccountRole) bool {
-	return priority[a] > priority[ar]
-}
+// type Table interface {
+// 	TableName() string
+// }
 
-func (a AccountRole) IsValid() bool {
-	_, ok := priority[a]
-	return ok
-}
+// func (Albums) TableName() string {
+// 	return "albums"
+// }
 
-// Status enum
-type Status byte
+// func (Artists) TableName() string {
+// 	return "artists"
+// }
 
-const (
-	InitialStatus = iota
-	WaitingForScanStatus
-	ScanningStatus
-	ScannedStatus
-)
+// func (Interactions) TableName() string {
+// 	return "interactions"
+// }
 
-type Table interface {
-	TableName() string
-}
+// func (PlayLists) TableName() string {
+// 	return "playlists"
+// }
 
-func (Albums) TableName() string {
-	return "albums"
-}
+// func (PlayListSongs) TableName() string {
+// 	return "playlist_songs"
+// }
 
-func (Artists) TableName() string {
-	return "artists"
-}
+// func (Songs) TableName() string {
+// 	return "songs"
+// }
 
-func (Interactions) TableName() string {
-	return "interactions"
-}
-
-func (PlayLists) TableName() string {
-	return "playlists"
-}
-
-func (PlayListSongs) TableName() string {
-	return "playlist_songs"
-}
-
-func (Songs) TableName() string {
-	return "songs"
-}
-
-func (Accounts) TableName() string {
-	return "accounts"
-}
+// func (Accounts) TableName() string {
+// 	return "accounts"
+// }
